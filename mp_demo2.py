8e3417818ca07038155f86afceca3a4373425082
Terry N. Brown <terrynbrown@gmail.com>
2019-08-14 12:36:34 -0500

    docstrings / comments

--- mp_demo2.py
+++ mp_demo2.py
@@ -1,9 +1,12 @@
+"""Demo multiprocessing a NumPy shared memory array"""
+
 import numpy as np
 import time
 from multiprocessing import Array, JoinableQueue, Process
 
 
 def setup(queue, shared_grid, rows, cols):
+    """make NumPy array from shared array and start watching queue"""
     global grid
     grid = np.frombuffer(shared_grid)
     grid.shape = (rows, cols)
@@ -11,6 +14,7 @@ def setup(queue, shared_grid, rows, cols):
 
 
 def proc_queue(queue):
+    """process tasks from queue until an exit task is seen"""
     while True:
         task = queue.get()
         if task['task'] == 'exit':
@@ -21,6 +25,7 @@ def proc_queue(queue):
 
 
 def inc_rows(start, end):
+    """process rows start...end in shared array"""
     print("Doing %s-%s" % (start, end))
     for row in range(start, end):
         for col in range(grid.shape[1]):
@@ -29,6 +34,7 @@ def inc_rows(start, end):
 
 
 def do_task(task):
+    """direct a task to the appropriate function"""
     if task['task'] == 'inc_rows':
         inc_rows(task['data']['start'], task['data']['end'])
     else:
@@ -41,7 +47,9 @@ if __name__ == '__main__':
 
     rows, cols = 10, 10
 
+    # create shared array
     shared_grid = Array('d', rows * cols, lock=False)
+    # and regular NumPy array using shared array memory
     grid = np.frombuffer(shared_grid)
     grid.shape = (rows, cols)
 
@@ -61,8 +69,11 @@ if __name__ == '__main__':
         })
     queue.join()
     print("done in %s" % (time.time() - start))
-    answer0 = np.array(grid)
-    grid[:] = 0
+    answer0 = np.array(grid)  # copy answer for comparison later
+
+    # end of first run, redo with one process to compare time
+
+    grid[:] = 0  # zero out grid again
     start = time.time()
     queue.put({
         'task': 'inc_rows',
@@ -72,7 +83,7 @@ if __name__ == '__main__':
     print("done in %s" % (time.time() - start))
     answer1 = np.array(grid)
 
-    assert (answer0 == answer1).all()
+    assert (answer0 == answer1).all()  # check answers match
 
-    for i in range(2):
+    for i in range(2):  # tell both processess to exit
         queue.put({'task': 'exit'})
